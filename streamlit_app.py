65bdadb659318faeeae568fee653739cc621f261
Lee Hampton <117728979+Beanbag006@users.noreply.github.com>
2022-11-10 10:11:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
     
+    
  except URLError as e:
     streamlit.error()
 
